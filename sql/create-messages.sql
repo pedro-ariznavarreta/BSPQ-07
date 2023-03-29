f8892e580be754b760e70b384b132a0f71ad5c90
Iñigo Susilla <insusilla@opendeusto.es>
2023-03-29 11:45:18 +0200

    Cambio nombre de la base de datos

--- sql/create-messages.sql
+++ sql/create-messages.sql
@@ -1,11 +1,11 @@
 /* DELETE 'messagesDB' database*/
-DROP SCHEMA IF EXISTS messagesDB;
+DROP SCHEMA IF EXISTS usuariosDB;
 /* DELETE USER 'spq' AT LOCAL SERVER*/
 DROP USER IF EXISTS 'spq'@'localhost';
 
-/* CREATE 'messagesDB' DATABASE */
-CREATE SCHEMA messagesDB;
+/* CREATE 'usuariosDB' DATABASE */
+CREATE SCHEMA usuariosDB;
 /* CREATE THE USER 'spq' AT LOCAL SERVER WITH PASSWORD 'spq' */
 CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
 
-GRANT ALL ON messagesDB.* TO 'spq'@'localhost';
+GRANT ALL ON usuariosDB.* TO 'spq'@'localhost';
