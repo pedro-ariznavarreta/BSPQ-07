3feb288565ff997c32fca08aa3ab58d09291c1e7
Pedro Ariznavarreta <pedro.ariznavarreta@opendeusto.es>
2023-04-29 12:36:36 +0200

    Cambio del pom para empezar con los test, eliminacion de metodos innecesarios en el resource, creacion de la clase Main en el paquete server para empezar con el test y creación de productotest

--- src/main/java/es/deusto/spq/supermarket/server/Resource.java
+++ src/main/java/es/deusto/spq/supermarket/server/Resource.java
@@ -49,81 +49,7 @@ public class Resource {
 		this.tx = pm.currentTransaction();
 	}
 
-	@POST
-	@Path("/sayMessage")
-	public Response sayMessage(DirectMessage directMessage) {
-		User user = null;
-		try {
-			tx.begin();
-			logger.info("Creating query ...");
 
-			try (Query<?> q = pm.newQuery("SELECT FROM " + User.class.getName() + " WHERE login == \""
-					+ directMessage.getUserData().getLogin() + "\" &&  password == \""
-					+ directMessage.getUserData().getPassword() + "\"")) {
-				q.setUnique(true);
-				user = (User) q.execute();
-
-				logger.info("User retrieved: {}", user);
-				if (user != null) {
-					Message message = new Message(directMessage.getMessageData().getMessage());
-					user.getMessages().add(message);
-					pm.makePersistent(user);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			tx.commit();
-		} finally {
-			if (tx.isActive()) {
-				tx.rollback();
-			}
-		}
-
-		if (user != null) {
-			cont++;
-			logger.info(" * Client number: {}", cont);
-			MessageData messageData = new MessageData();
-			messageData.setMessage(directMessage.getMessageData().getMessage());
-			return Response.ok(messageData).build();
-		} else {
-			return Response.status(Status.BAD_REQUEST)
-					.entity("Login details supplied for message delivery are not correct").build();
-		}
-	}
-
-	@POST
-	@Path("/register")
-	public Response registerUser(UserData userData) {
-		try {
-			tx.begin();
-			logger.info("Checking whether the user already exits or not: '{}'", userData.getLogin());
-			User user = null;
-			try {
-				user = pm.getObjectById(User.class, userData.getLogin());
-			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
-				logger.info("Exception launched: {}", jonfe.getMessage());
-			}
-			logger.info("User: {}", user);
-			if (user != null) {
-				logger.info("Setting password user: {}", user);
-				user.setPassword(userData.getPassword());
-				logger.info("Password set user: {}", user);
-			} else {
-				logger.info("Creating user: {}", user);
-				user = new User(userData.getLogin(), userData.getPassword(), userData.getEsTrabajador(),
-						userData.getEsAdministrador());
-				pm.makePersistent(user);
-				logger.info("User created: {}", user);
-			}
-			tx.commit();
-			return Response.ok().build();
-		} finally {
-			if (tx.isActive()) {
-				tx.rollback();
-			}
-
-		}
-	}
 
 	@GET
 	@Path("/usuarios")
@@ -198,7 +124,7 @@ public class Resource {
 	}
 
 	@POST
-	@Path("elim")
+	@Path("/elim")
 	@Produces(MediaType.APPLICATION_JSON)
 	public static void eliminarUsuario(List<String> usuarioL) {
 		String nick = usuarioL.get(0);
@@ -216,7 +142,7 @@ public class Resource {
 	}
 
 	@GET
-	@Path("nomcheck")
+	@Path("/nomcheck")
 	@Produces(MediaType.APPLICATION_JSON)
 	public static boolean nomcheck(@QueryParam("nick") String nick) {
 		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
---
+++ src/test/java/es/deusto/spq/supermarket/server/jdo/ProductoTest.java
@@ -0,0 +1,77 @@
+package es.deusto.spq.supermarket.server.jdo;
+
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProductoTest {
+
+	private Producto p;
+	
+	@Before
+	public void crearProducto() {
+		p = new Producto("Platano", "Deliciosa", 3, "pedro", 100);
+		
+	}
+	
+	@Test
+	public void testGetNombre() {
+		assertEquals("Platano", p.getNombre());
+	}
+	
+	@Test
+	public void testGetDescipcion() {
+		assertEquals("Deliciosa", p.getDescripcion());
+	}
+	
+	@Test
+	public void testGetPrecio() {
+		assertEquals(3, p.getPrecio(), 0.001);
+	}
+	
+	@Test
+	public void testGetUsuario() {
+		assertEquals("pedro", p.getUsuario());
+	}
+	
+	@Test
+	public void testGetCantidad() {
+		assertEquals(100, p.getCantidad());
+	}
+
+	
+	@Test
+	public void testSetNombre() {
+		p.setNombre("Melo");
+		assertEquals("Melo", p.getNombre());
+	}
+	
+	@Test
+	public void testSetDescipcion() {
+		p.setDescripcion("Rica");
+		assertEquals("Rica", p.getDescripcion());
+	}
+	
+	@Test
+	public void testSetPrecio() {
+		p.setPrecio(2.6);
+		assertEquals(2.6, p.getPrecio(), 0.001);
+	}
+	
+	@Test
+	public void testSetUsuario() {
+		p.setUsuario("javi");
+		assertEquals("javi", p.getUsuario());
+	}
+	
+	@Test
+	public void testSetCantidad() {
+		p.setCantidad(10);
+		assertEquals(10, p.getCantidad());
+	}
+
+	
+}
