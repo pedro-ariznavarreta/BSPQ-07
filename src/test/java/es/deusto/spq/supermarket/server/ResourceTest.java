1e91ba3a74e2cbff06950c65c72fc342465360c4
Pedro Ariznavarreta <pedro.ariznavarreta@opendeusto.es>
2023-05-01 11:08:58 +0200

    Metodo Junit para obtener todos los productos

--- src/test/java/es/deusto/spq/supermarket/server/ResourceTest.java
+++ src/test/java/es/deusto/spq/supermarket/server/ResourceTest.java
@@ -2,13 +2,66 @@ package es.deusto.spq.supermarket.server;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+import categories.IntegrationTest;
+import es.deusto.spq.supermarket.server.jdo.Product;
+
+@Category(IntegrationTest.class)
 public class ResourceTest {
+	
+	
+	private HttpServer server;
+    private WebTarget appTarget;
+    private WebTarget usuarioTarget;
+    private Client c;
+    
+    @Before
+    public void setUp() throws Exception {
+    	
+    	server = Main.startServer();
+        Client c = ClientBuilder.newClient();
+      
+        appTarget = c.target(Main.BASE_URI);
+        
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
+    
+    
+    
+    @Test
+    public void testGetIt() {
+    	WebTarget productTarget = appTarget.path("resource");
+	    WebTarget productAllTarget = productTarget.path("allP");
+    	
+    	List<Product> listProd = Arrays.asList(
+    			new Product("Lechuga", "Muy sana", 2.4, "unai", 6),
+    			new Product("Platano", "Deliciosa", 3, "pedro", 100),
+    			new Product("Mango", "Recien horneado", 0.6, "javi", 75));
 
-	@Test
-	public void test() {
-		
-	}
+    	GenericType<List<Product>> genericType = new GenericType<List<Product>>() {};
+    	List<Product> productos = productAllTarget.request(MediaType.APPLICATION_JSON).get(genericType);
+    	
+        assertEquals(listProd.get(0).getNombre(), productos.get(0).getNombre());
+        assertEquals(listProd.get(1).getNombre(), productos.get(1).getNombre());
+        assertEquals(listProd.get(2).getNombre(), productos.get(2).getNombre());
+    }
 
 }
