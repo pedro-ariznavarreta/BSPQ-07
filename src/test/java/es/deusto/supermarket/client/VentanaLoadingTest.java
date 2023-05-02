07d41f5e9efe2d1f22a9ebafc47e0e0de974579d
Javier Pombo <jpombourrutia@opendeusto.es>
2023-05-02 15:03:36 +0200

    Test Ventanaloading

--- src/test/java/es/deusto/supermarket/client/VentanaLoadingTest.java
+++ src/test/java/es/deusto/supermarket/client/VentanaLoadingTest.java
@@ -1,6 +1,11 @@
 package es.deusto.supermarket.client;
 
 
+import static org.junit.Assert.assertEquals;
+
+import javax.swing.JProgressBar;
+
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 
@@ -10,8 +15,11 @@ import categories.PerformanceTest;
 
 @Category(PerformanceTest.class)
 public class VentanaLoadingTest {
-
-	
-	
+	@Test
+	public void valor100Test() {
+		JProgressBar bar = new JProgressBar();
+		bar.setValue(100);
+		assertEquals(100, bar.getValue());
+	}
 
 }
