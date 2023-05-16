ff306e100d47daef121032cac84f7ddc9662e854
Javier Pombo <jpombourrutia@opendeusto.es>
2023-05-16 11:27:33 +0200

    Metodos de los test de integración

--- src/test/java/es/deusto/spq/supermarket/server/ServerIntegrationTest.java
+++ src/test/java/es/deusto/spq/supermarket/server/ServerIntegrationTest.java
@@ -1,6 +1,7 @@
 package es.deusto.spq.supermarket.server;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +23,6 @@ import org.junit.experimental.categories.Category;
 
 import com.github.noconnor.junitperf.JUnitPerfRule;
 import com.github.noconnor.junitperf.JUnitPerfTest;
-import com.github.noconnor.junitperf.JUnitPerfTestRequirement;
 import com.github.noconnor.junitperf.reporting.providers.HtmlReportGenerator;
 
 import categories.IntegrationTest;
@@ -105,4 +105,68 @@ public class ServerIntegrationTest {
 
 		assertEquals(false, usuario);
 	}
+	
+	
+	@Test
+
+	public void testGetIt() {
+	    WebTarget productAllTarget = appTarget.path("allP");
+		
+		List<Product> listProd = Arrays.asList(
+				new Product("Lechuga", "Muy sana", 2.4, "unai",6),
+				new Product("Platano", "Deliciosa", 3, "pedro",100),
+				new Product("Mango", "Recien horneado", 0.6, "javi",75));
+
+		GenericType<List<Product>> genericType = new GenericType<List<Product>>() {};
+		List<Product> productos = productAllTarget.request(MediaType.APPLICATION_JSON).get(genericType);
+		
+	    assertEquals(listProd.get(0).getNombre(), productos.get(0).getNombre());
+	    assertEquals(listProd.get(1).getNombre(), productos.get(1).getNombre());
+	    assertEquals(listProd.get(2).getNombre(), productos.get(2).getNombre());
+	}
+	@Test
+	public void testgetNombreProductos() {
+	    WebTarget productNomTarget = appTarget.path("nomP").queryParam("nombre", "Lechuga");
+	    
+	    List<Product> listProd = Arrays.asList(
+				new Product("Lechuga", "Muy sana", 2.4, "unai",6),
+				new Product("Platano", "Deliciosa", 3, "pedro",100),
+				new Product("Mango", "Recien horneado", 0.6, "javi",75));
+	    
+	    GenericType<List<Product>> genericType = new GenericType<List<Product>>() {};
+	    List<Product> producto = productNomTarget.request(MediaType.APPLICATION_JSON).get(genericType);
+	    
+	    assertEquals(listProd.get(0).getNombre(), producto.get(0).getNombre());
+	}
+
+
+	@Test
+
+	public void testAñadirProductoCesta() {
+		Product p = new Product("peras", "muy dulces", 0.5, "unai", 10);
+		WebTarget cestaAñadirTarget = appTarget.path("anadir").queryParam("Producto", p.getNombre()).queryParam("Usuario", "pedro");
+		
+		GenericType<Boolean> genericType5 = new GenericType<Boolean>() {};
+		boolean respuesta = cestaAñadirTarget.request(MediaType.APPLICATION_JSON).get(genericType5);
+		
+		assertEquals(true, respuesta);
+		
+	}
+
+
+	@Test
+
+	public void testVaciarCesta() {
+		WebTarget cestaBorrarTarget = appTarget.path("borrar");
+		Usuario usuario = new Usuario("pedro", "1234", null, 0, 0);
+		cestaBorrarTarget.request().post(Entity.entity(usuario, MediaType.APPLICATION_JSON));
+		
+		WebTarget cestaBuscarTarget = appTarget.path("buscar").queryParam("Usuario", "pedro");
+		GenericType<List<Product>> genericType = new GenericType<List<Product>>() {};
+		List<Product> producto = cestaBuscarTarget.request(MediaType.APPLICATION_JSON).get(genericType);
+		
+		assertTrue(producto.isEmpty());
+		
+	}	
+	
 }
