c9b54beb3becec69bd6f29f7421117ab080d40b0
javier-ramos2002 <javier-ramos@opendeusto.es>
2023-05-22 11:37:39 +0200

    Botón eliminar producto de la cesta

--- src/main/java/es/deusto/spq/supermarket/client/VentanaCesta.java
+++ src/main/java/es/deusto/spq/supermarket/client/VentanaCesta.java
@@ -194,35 +194,75 @@ public class VentanaCesta extends JFrame {
 
 			}
 		});
+		
+		JButton btnEliminarProducto = new JButton("ELIMINAR PRODUCTO");
+		btnEliminarProducto.setBackground(Color.YELLOW);
+		btnEliminarProducto.addActionListener(new ActionListener() {
+		    	public void actionPerformed(ActionEvent e) {
+		            int indiceSeleccionado = list.getSelectedIndex();
+		            if (indiceSeleccionado != -1) {
+		                DefaultListModel<Product> modeloLista = (DefaultListModel<Product>) list.getModel();
+		                Product productoSeleccionado = modeloLista.getElementAt(indiceSeleccionado);
+
+		                // Envía una solicitud de eliminación al servidor
+		                WebTarget eliminarTarget = appTarget.path("eliminar");
+		                eliminarTarget.request().post(Entity.entity(productoSeleccionado.getCodigo(), MediaType.APPLICATION_JSON));
+
+		                // Elimina el producto de la lista
+		                modeloLista.remove(indiceSeleccionado);
+		            }
+		    	}
+		    });
 
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
-				.createSequentialGroup()
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup().addContainerGap().addComponent(lblCesta,
-								GroupLayout.DEFAULT_SIZE, 634, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup().addGap(216)
-								.addComponent(btnComprar, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.UNRELATED).addComponent(btnVaciarCesta,
-										GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup().addGap(50)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(list, GroupLayout.PREFERRED_SIZE, 562, GroupLayout.PREFERRED_SIZE)
-										.addGroup(groupLayout.createSequentialGroup().addComponent(volver)
-												.addPreferredGap(ComponentPlacement.RELATED, 369, Short.MAX_VALUE)
-												.addComponent(btnNewButton).addGap(65)))))
-				.addContainerGap()));
-		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
-				.createSequentialGroup().addGap(8)
-				.addComponent(lblCesta, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
-				.addPreferredGap(ComponentPlacement.UNRELATED)
-				.addComponent(list, GroupLayout.PREFERRED_SIZE, 243, GroupLayout.PREFERRED_SIZE).addGap(18)
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnComprar, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnVaciarCesta, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(ComponentPlacement.RELATED, 25, Short.MAX_VALUE).addGroup(groupLayout
-						.createParallelGroup(Alignment.BASELINE).addComponent(volver).addComponent(btnNewButton))
-				.addContainerGap()));
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblCesta, GroupLayout.DEFAULT_SIZE, 636, Short.MAX_VALUE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(50)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(list, GroupLayout.PREFERRED_SIZE, 562, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(volver)
+									.addPreferredGap(ComponentPlacement.RELATED, 391, Short.MAX_VALUE)
+									.addComponent(btnNewButton)
+									.addGap(65)))))
+					.addContainerGap())
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(120)
+					.addComponent(btnComprar, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+					.addComponent(btnEliminarProducto, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
+					.addGap(34)
+					.addComponent(btnVaciarCesta, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
+					.addGap(127))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(8)
+					.addComponent(lblCesta, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(list, GroupLayout.PREFERRED_SIZE, 243, GroupLayout.PREFERRED_SIZE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnComprar, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnVaciarCesta, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(volver)
+								.addComponent(btnNewButton)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(18)
+							.addComponent(btnEliminarProducto, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+		);
 		getContentPane().setLayout(groupLayout);
 	}
 }
